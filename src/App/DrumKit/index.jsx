23e39b7e11c20870f2ef9a0b7d9e238e2603b841
Iwona <iwonkabigos@gmail.com>
2024-02-22 14:30:53 +0100

    Started project, added routes to pages and style for DrumKey component in DrumKit page

---
+++ src/App/DrumKit/index.jsx
@@ -0,0 +1,11 @@
+import "./style.css";
+import { DrumKey } from "./DrumKey";
+
+export function DrumKit() {
+  // const drumKeys = [{key = "", audio = ""}];
+  return (
+    <>
+      <DrumKey keyName="A" keySound="Clap" />
+    </>
+  );
+}
