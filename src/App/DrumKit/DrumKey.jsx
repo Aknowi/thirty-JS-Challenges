1542389e845c411d8677c8a477663ee0fdc4bb2c
Iwona <iwonkabigos@gmail.com>
2024-02-22 18:25:11 +0100

    added drumelements and its rendering functionality

--- src/App/DrumKit/DrumKey.jsx
+++ src/App/DrumKit/DrumKey.jsx
@@ -1,10 +1,13 @@
 import "./style.css";
 
-export function DrumKey({ keyName, keySound }) {
+export function DrumKey({ keyName, keySound, keySrc, audioID }) {
   return (
     <div className="drum-key">
       <h3>{keyName}</h3>
       <p>{keySound}</p>
+      <audio id={audioID}>
+        <source src={keySrc} />
+      </audio>
     </div>
   );
 }
--- src/App/DrumKit/index.jsx
+++ src/App/DrumKit/index.jsx
@@ -1,11 +1,87 @@
 import "./style.css";
 import { DrumKey } from "./DrumKey";
+import { useEffect, useState } from "react";
+
+const drumElements = [
+  {
+    name: "a",
+    soundName: "clap",
+    keyCode: "65",
+    audioSrc: "./ImagesFontsAudio/clap.wav",
+  },
+  {
+    name: "s",
+    soundName: "hihat",
+    keyCode: "83",
+    audioSrc: "./ImagesFontsAudio/hihat.wav",
+  },
+  {
+    name: "d",
+    soundName: "kick",
+    keyCode: "68",
+    audioSrc: "./ImagesFontsAudio/kick.wav",
+  },
+  {
+    name: "f",
+    soundName: "openhat",
+    keyCode: "70",
+    audioSrc: "./ImagesFontsAudio/openhat.wav",
+  },
+  {
+    name: "g",
+    soundName: "boom",
+    keyCode: "71",
+    audioSrc: "./ImagesFontsAudio/boom.wav",
+  },
+  {
+    name: "h",
+    soundName: "ride",
+    keyCode: "72",
+    audioSrc: "./ImagesFontsAudio/ride.wav",
+  },
+  {
+    name: "j",
+    soundName: "snare",
+    keyCode: "74",
+    audioSrc: "./ImagesFontsAudio/snare.wav",
+  },
+  {
+    name: "k",
+    soundName: "tom",
+    keyCode: "75",
+    audioSrc: "./ImagesFontsAudio/tom.wav",
+  },
+  {
+    name: "l",
+    soundName: "tink",
+    keyCode: "76",
+    audioSrc: "./ImagesFontsAudio/tink.wav",
+  },
+];
 
 export function DrumKit() {
-  // const drumKeys = [{key = "", audio = ""}];
+  const handleOnKey = (event) => {
+    const key = event.key;
+    const matchedElement = drumElements.find((element) => element.name === key);
+    if (matchedElement) {
+      let audio = new Audio(matchedElement.audioSrc);
+      audio.play();
+    }
+  };
+
   return (
-    <>
-      <DrumKey keyName="A" keySound="Clap" />
-    </>
+    <div tabIndex="0" onKeyDown={handleOnKey}>
+      {drumElements.map((element, index) => {
+        return (
+          <DrumKey
+            keyName={element.name}
+            keySound={element.soundName}
+            key={index}
+            keySrc={element.audioSrc}
+            audioID={index}
+          />
+        );
+      })}
+    </div>
   );
 }
