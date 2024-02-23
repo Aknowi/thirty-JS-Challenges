94acbe221681e763e74d230920c77d99767ebf71
Iwona <iwonkabigos@gmail.com>
2024-02-23 18:51:32 +0100

    added initial css to dashboard

--- src/App/Dashboard/index.jsx
+++ src/App/Dashboard/index.jsx
@@ -1,9 +1,15 @@
+// import { DrumKit } from "./Dashboard/leaves.png";
+
+import { NavLink } from "react-router-dom";
+
 export function Dashboard() {
   // Stworzyć buttony powrotu dla każdej strony do dashboard
 
   return (
-    <>
-      <p>lista challengy</p>
-    </>
+    <div className="dashboard-box">
+      <div className="challenges-box">
+        <NavLink to="/drum-kit">Challange 1: Drum Kit</NavLink>
+      </div>
+    </div>
   );
 }
