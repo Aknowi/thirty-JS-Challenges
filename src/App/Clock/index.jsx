7353634ffa5a7e91d79be7a55608df8141d277fd
Iwona <iwonkabigos@gmail.com>
2024-03-05 18:18:36 +0100

    added functionality to clock, stylized clock hands

--- src/App/Clock/index.jsx
+++ src/App/Clock/index.jsx
@@ -1,16 +1,48 @@
+"use strict";
+
+import { useState } from "react";
 import "./style.css";
+import { useEffect } from "react";
 
 // - zmienić z rem na px
 // - dodać RWD
-// - dodać funkcjonalność zegara
+// - dodać z-index
+// - dodać border color
 
 export function Clock() {
+  const [secondDegree, setSecondDegree] = useState(90);
+  const [minuteDegree, setMinuteDegree] = useState(90);
+  const [hourDegree, setHourDegree] = useState(90);
+
+  useEffect(() => {
+    const intervalID = setInterval(() => {
+      const now = new Date();
+      const nowSecond = now.getSeconds();
+      const nowMinute = now.getMinutes();
+      const nowHour = now.getHours();
+
+      setSecondDegree(nowSecond * 6 + 90);
+      setMinuteDegree(nowMinute * 6 + nowSecond * 0.1 + 90);
+      setHourDegree(nowHour * 30 + nowMinute * (0.1 / 12) + 90);
+    }, 1000);
+    return () => clearInterval(intervalID);
+  }, []);
+
   return (
     <div className="clock-page">
       <div className="clock-frame">
-        <div className="clock-hand hour"></div>
-        <div className="clock-hand minute"></div>
-        <div className="clock-hand second"></div>
+        <div
+          className="clock-hand hour"
+          style={{ transform: `rotate(${hourDegree}deg)` }}
+        ></div>
+        <div
+          className="clock-hand minute"
+          style={{ transform: `rotate(${minuteDegree}deg)` }}
+        ></div>
+        <div
+          className="clock-hand second"
+          style={{ transform: `rotate(${secondDegree}deg)` }}
+        ></div>
       </div>
     </div>
   );
