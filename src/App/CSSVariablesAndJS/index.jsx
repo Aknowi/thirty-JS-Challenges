65744ed0c152e3cbf659167ad7ac0f957de72fef
Iwona <iwonkabigos@gmail.com>
2024-04-09 21:39:07 +0200

    added CSS veriables, added changing dynamically CSS values onChange

--- src/App/CSSVariablesAndJS/index.jsx
+++ src/App/CSSVariablesAndJS/index.jsx
@@ -5,16 +5,55 @@ import bird from "./bird.jpg";
 export function CSSVariablesAndJS() {
   return (
     <div className="variables-page">
-      <h1>Update CSS Variables with JS</h1>
+      <h1>
+        Update CSS Variables with <span className="colored-text">JS</span>
+      </h1>
       <div className="input-box">
         <label id="spacing">Spacing: </label>
-        <input type="range" id="spacing" name="spacing"></input>
+        <input
+          type="range"
+          min="10"
+          max="200"
+          id="spacing"
+          name="spacing"
+          defaultValue="10"
+          onChange={(e) => {
+            document.documentElement.style.setProperty(
+              "--spacing",
+              `${e.target.value}px`
+            );
+          }}
+        ></input>
 
         <label id="blur">Blur: </label>
-        <input type="range" id="blur" name="blur"></input>
+        <input
+          type="range"
+          min="0"
+          max="25"
+          id="blur"
+          name="blur"
+          defaultValue="0"
+          onChange={(e) => {
+            document.documentElement.style.setProperty(
+              "--blur",
+              `${e.target.value}px`
+            );
+          }}
+        ></input>
 
         <label id="base-color">Base color: </label>
-        <input type="color" id="base-color" name="base-color"></input>
+        <input
+          type="color"
+          id="base-color"
+          name="base-color"
+          defaultValue="#17964c"
+          onChange={(e) => {
+            document.documentElement.style.setProperty(
+              "--base-color",
+              e.target.value
+            );
+          }}
+        ></input>
       </div>
       <img className="bird-img" src={bird} alt="small-bird" />
     </div>
